71a7246d0f2da511b8eb8a16fd8874270fdc2ba7
liuchichang <chichang.tw@outlook.com>
2021-09-30 02:46:43 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   // publicPath:'',   //process.env.NODE_ENV === "production" ? "/banner-project" : "/",
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/kz_foxconn/' // test20200915 為 repo 名稱
+    ? '/foxconn/' 
     : '/',
   devServer: {
     port: 8080,

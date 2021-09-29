6f5b84db25d28112cd21205b23d85c324ffc84eb
liuchichang <chichang.tw@outlook.com>
2021-09-30 02:30:39 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,9 @@ module.exports = {
     'resize-detector'
   ],
   // publicPath:'',   //process.env.NODE_ENV === "production" ? "/banner-project" : "/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/kz_foxconn/' // test20200915 為 repo 名稱
+    : '/',
   devServer: {
     port: 8080,
   },
